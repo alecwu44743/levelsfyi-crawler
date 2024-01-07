f742ba37d6cbf3dee700f3b76eae9b99f870169b
alecwu44743 <alec.wu44743@gmail.com>
2024-01-07 21:14:24 +0800

    Add unit tests for query function

--- test_levelsfyi_crawler_nonarg.py
+++ test_levelsfyi_crawler_nonarg.py
@@ -109,3 +109,51 @@ def test_query_min_tc():
                     and "Stock(yr): $40,000" in mock_stdout.getvalue() \
                     and "Bonus: $10,000" in mock_stdout.getvalue() \
                     and "Company: Facebook" not in mock_stdout.getvalue()
+
+def test_query_median_tc():
+    levels_data = [
+        {
+            'Company': 'Google',
+            'Location': 'Sunnyvale, CA',
+            'Date': '2024-01-07', 
+            'Level Name': 'L4',
+            'Tag': 'Software',
+            'YearOfExperience': '5 years',
+            'Total/AtCompany': '0',  
+            'TotalCompensation': '$170,000',
+            'Base': '$120,000',
+            'Stock(yr)': '$40,000',
+            'Bonus': '$10,000'
+        },
+        {
+            'Company': 'Netflix',
+            'Location': 'Los Gatos, CA',
+            'Date': '2024-01-07',
+            'Level Name': 'L5',
+            'Tag': 'AI/ML',
+            'YearOfExperience': '5 years',
+            'Total/AtCompany': '0',
+            'TotalCompensation': '$300,000',
+            'Base': '$200,000',
+            'Stock(yr)': '$80,000',
+            'Bonus': '$20,000'
+        },
+        {
+            'Company': 'Facebook',
+            'Location': 'Menlo Park, CA',
+            'Date': '2024-01-07',
+            'Level Name': 'E4',
+            'Tag': 'Software',
+            'YearOfExperience': '5 years',
+            'Total/AtCompany': '0',
+            'TotalCompensation': '$200,000',
+            'Base': '$120,000',
+            'Stock(yr)': '$60,000',
+            'Bonus': '$20,000'
+        }
+    ]
+    with patch('builtins.input', side_effect=['median_tc', 'exit']):
+        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
+            query(levels_data)
+            assert "[v] Median total compensation" in mock_stdout.getvalue() \
+                    and "200000" in mock_stdout.getvalue()
